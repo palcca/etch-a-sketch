227bbe2a483a7001b17d29d9fb86de5bb77a14e5
Palcca <dgabor90@gmail.com>
2024-01-15 15:01:53 +0100

    First version of Etch a sketch project

---
+++ script.js
@@ -0,0 +1,37 @@
+let canvas = 1;
+
+function makeGrid(size){
+    for (x=1;x<=size;x++) {
+        canvas = document.querySelector("#canvas");
+        const gridLine = document.createElement("div");
+        gridLine.setAttribute("class","gridLine");
+        canvas.appendChild(gridLine);
+        for (y=1; y<=size; y++){
+            const gridColumn=document.createElement("div");
+            gridColumn.textContent = " ";
+            const paddingSize = (960/size/2);
+            gridColumn.style = "padding: " + paddingSize+"px";
+            gridColumn.setAttribute("class","gridColumn");
+            gridColumn.addEventListener("mouseover", () =>{
+                gridColumn.style.backgroundColor = "black";
+            });
+            gridLine.appendChild(gridColumn);
+        }        
+    }
+}
+const btnSize = document.querySelector("#btnSize");
+
+btnSize.addEventListener("click", () => {
+    if (canvas!=1){
+        while(canvas.firstChild){
+            canvas.removeChild(canvas.firstChild);
+        }
+    }
+    let gridSize=prompt("Canvas grid size:", "MAX 100");
+    while (gridSize>100){
+        alert("That's too big for me!")
+        gridSize=prompt("Canvas grid size:", "MAX 100");
+    }
+    makeGrid(gridSize);
+})
+
