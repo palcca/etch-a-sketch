29a9615729ccada72c81b71c3d15cfda4b971ce5
Palcca <dgabor90@gmail.com>
2024-01-15 15:24:41 +0100

    Better style

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let canvas = 1;
 
 function makeGrid(size){
+    canvas = document.querySelector("#canvas");
     for (x=1;x<=size;x++) {
-        canvas = document.querySelector("#canvas");
         const gridLine = document.createElement("div");
         gridLine.setAttribute("class","gridLine");
         canvas.appendChild(gridLine);
         for (y=1; y<=size; y++){
             const gridColumn=document.createElement("div");
-            gridColumn.textContent = " ";
             const paddingSize = (960/size/2);
-            gridColumn.style = "padding: " + paddingSize+"px";
+            gridColumn.style = "padding: " + paddingSize + "px";
             gridColumn.setAttribute("class","gridColumn");
             gridColumn.addEventListener("mouseover", () =>{
                 gridColumn.style.backgroundColor = "black";
